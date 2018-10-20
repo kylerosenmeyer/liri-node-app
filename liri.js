49754f2fd27de226555d0ab3cd83d486eaf13e59
Kyle Rosenmeyer <kylerosenmeyer@gmail.com>
2018-10-20 16:37:28 -0400

    asethetics update

--- liri.js
+++ liri.js
@@ -4,7 +4,7 @@ require("dotenv").config()
 //Use "concert-this" + artist/band to perform a concert search
 //Use "spotify-this-song" + song name to perform a music search
 //Use "movie-this" + movie title to perform a movie search
-//Use "do what it says" to let liri perform a search of it's choice.
+//Use "do what it says" to let liri perform it's favorite searches.
 
 //Declare a variable for the userRequest type and the content of the userRequest,
 //Also Declare constanst to require the installed NPMs
@@ -22,13 +22,16 @@ const request = require("request"),
       safe = require("../liri-node-app/keys.js"),
       fs = require("fs")
 
-//Run through multiple words if any add to the content variable.
+//Run through multiple words if any, and add to the content variable.
 for ( let i=4; i<process.argv.length; i++ ) {
     entertainment += " " + process.argv[i]
     artist += "%20" + process.argv[i]
     movietitle += "+" + process.argv[i]
 }
 
+//The main functions here; concerts, music, movies; run the api requests.
+//Another function runs the log (logActivity)
+//Concerts is the BandsInTown api function
 var concerts = function() {
         var band = {
             apikey: safe.bandintown.apikey
@@ -63,7 +66,9 @@ var concerts = function() {
                 }
             })
         },
-    music = function() {
+    //Music is the Spotify API function
+    music = function(e) {
+
         //This variable grabs the ids from the external .js file
         var spotify = new Spotify ({
             id: safe.spotify.id,
@@ -73,7 +78,7 @@ var concerts = function() {
         //This is the api request
         spotify.search({
             type: "track", 
-            query: entertainment,
+            query: e,
             limit: "1" 
         }, function(err, data) {
 
@@ -93,12 +98,13 @@ var concerts = function() {
             }
         })
         },
-    movies = function() {
+    //Movies is the OMDB API function
+    movies = function(e) {
         if ( process.argv[3] === undefined ) {
-            movietitle = "Mr.+Nobody"
+            e = "Mr.+Nobody"
         }
     
-        let movieSearch = "http://www.omdbapi.com/?apikey=trilogy&t=" + movietitle,
+        let movieSearch = "http://www.omdbapi.com/?apikey=trilogy&t=" + e,
             options = {
                 url: movieSearch,
                 method: "GET",
@@ -124,11 +130,14 @@ var concerts = function() {
             }
         })
     },
-    logActivity = function () {
 
+    //LogActivity is the function that writes the log.txt file.
+    logActivity = function () { 
+
+        //If the user requested a concert, run the concert log.
         if (userRequest === "concert-this") {
 
-            var logConcert = ("\nThe next show is at " + log[0] + ". \nin " + log[1]+ ". \non " + log[2] + ".\n")
+            var logConcert = ("\nThe next show is at " + log[0] + ". \nCity: " + log[1]+ ". \nShow Date: " + log[2] + ".\nDate of search: " + moment().format("MM/DD/YYYY") + ".\n")
 
             fs.appendFile("log.txt", "\nArtist searched: " + entertainment + logConcert, function(err) {
 
@@ -142,9 +151,10 @@ var concerts = function() {
               });
 
               log = []
+        //If the user requested a Song, run the Spotify Log
         } else if (userRequest === "spotify-this-song") {
 
-            var logSong = ("\nArtist: " + log[0] + ". \nSong: " + log[1]+ ". \nAlbum: " + log[2] + ". \nPreview: " + log[3] + ".\n")
+            var logSong = ("\nArtist: " + log[0] + ". \nSong: " + log[1]+ ". \nAlbum: " + log[2] + ". \nPreview: " + log[3] + ".\nDate of search: " + moment().format("MM/DD/YYYY") + ".\n")
 
             fs.appendFile("log.txt", "\nSong searched: " + entertainment + logSong, function(err) {
 
@@ -158,8 +168,10 @@ var concerts = function() {
               });
 
               log = []
+
+        //if the user requested a movie, run the movie log.
         } else if (userRequest === "movie-this" ) {
-            var logMovie = ("\nTitle: " + log[0] + ". \nRelease Year: " + log[1] + ". \nRating: " + log[2] + ". \nRotton Tomatoes Score: " + log[3] + ". \nProduced In: " + log[4] + ". \nLanguage: " + log[5] + ". \nPlot: " + log[6] + ". \nActors: " + log[7] +".\n")
+            var logMovie = ("\nTitle: " + log[0] + ". \nRelease Year: " + log[1] + ". \nRating: " + log[2] + ". \nRotton Tomatoes Score: " + log[3] + ". \nProduced In: " + log[4] + ". \nLanguage: " + log[5] + ". \nPlot: " + log[6] + ". \nActors: " + log[7] +".\nDate of search: " + moment().format("MM/DD/YYYY") + ".\n")
             fs.appendFile("log.txt", "\nMovie searched: " + entertainment + logMovie, function(err) {
 
                 // If the code experiences any errors it will log the error to the console.
@@ -173,8 +185,9 @@ var concerts = function() {
         }
         
     }
+//This section runs the main condition check on what the user is asking for.
+//This triggers the functions above accordingly.
 
-// console.log("This is the entertainment: " + entertainment)
 //Check the first condition, a concert search.
 if ( userRequest === "do-what-it-says") {
     fs.readFile("random.txt", "utf8", function(error, data) {
@@ -185,36 +198,35 @@ if ( userRequest === "do-what-it-says") {
 
             setTimeout( function() {
 
-                console.log("\nThis is Liri's Favorite Song")
+                console.log("\nThis is Liri's Favorite Song\n")
                 var getData = data.split(",")
                 userRequest = getData[0]
                 entertainment = getData[1]
                 // console.log(entertainment)
-                music()
+                music(entertainment)
 
             }, 1000)
 
             setTimeout( function() {
 
-                console.log("\nThis is Liri's Favorite Concert Coming Up")
+                console.log("\nThis is Liri's Favorite Concert Coming Up\n")
                 var getData = data.split(",")
                 userRequest = getData[2]
                 artist = getData[3]
                 entertainment = artist
                 console.log(artist)
-                concerts()
+                concerts(artist)
 
             }, 2000)
 
             setTimeout( function() {
 
-                console.log("\nThis is Liri's Favorite Movie")
+                console.log("\nThis is Liri's Favorite Movie\n")
                 var getData = data.split(",")
                 userRequest = getData[4]
                 movietitle = getData[5]
-                entertianment = movietitle
-                // console.log(movietitle)
-                movies()
+                entertainment = movietitle
+                movies(entertainment)
 
             }, 3000)
             
@@ -225,15 +237,15 @@ if ( userRequest === "do-what-it-says") {
       });
 }else if ( userRequest === "concert-this" ) {
 
-    concerts()
+    concerts(artist)
 
 //Check the second condition, a song search.
 } else if ( userRequest === "spotify-this-song" ) {
     
-    music()
+    music(entertainment)
 
 //Check the third condition, a movie search.
 } else if ( userRequest === "movie-this" ) {
 
-   movies()
+   movies(movietitle)
 }
